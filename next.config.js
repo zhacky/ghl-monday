ff2f5b050575efdedbc4cbcda94d35f3faf840ac
Zhack <zhacky@gmail.com>
2024-01-11 18:46:58 +0800

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'clickcallsell.com',
+                port: '',
+                pathname: '/wp-content/**'
+            },
+        ],
+    }
+}
 
 module.exports = nextConfig
